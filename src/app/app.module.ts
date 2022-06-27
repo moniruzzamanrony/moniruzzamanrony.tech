d03dc9c86c5c3b142db366d3f626171960bb6975
Moniruzzaman Rony <eproni29@gmail.com>
2022-06-27 17:41:45 +0600

    Refresh project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeModule } from './components/home/home.module';
-import { GeneralModule } from './components/general/general.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AnimateOnScrollModule } from 'ng2-animate-on-scroll';
-import { HttpClientModule } from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeModule} from './components/home/home.module';
+import {GeneralModule} from './components/general/general.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AnimateOnScrollModule} from 'ng2-animate-on-scroll';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    /* ArchiveComponent */
+    AppComponent
   ],
   imports: [
     BrowserAnimationsModule,
@@ -26,4 +25,5 @@ import { HttpClientModule } from '@angular/common/http';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
